b2048c4aa28b6263c084aa37e8ce855b599c3915
Italo Simões <2d.italo@gmail.com>
2022-01-18 09:49:39 -0300

    header complete

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,18 @@
 import React from "react";
-import mylogo from "../logo.png";
-import { FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import { faBars } from  "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import Typed from "react-typed";
 
 const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
       <div className="container">
         <a className="navbar-brand" href="#">
-          <img className="logo" src={mylogo} alt="logo..." />
+          <Typed
+            className="typed-logo"
+            strings={["Ítalo Simões"]}
+            typeSpeed={40}
+          />
         </a>
         <button
           className="navbar-toggler"
@@ -19,15 +23,14 @@ const Navbar = () => {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <FontAwesomeIcon icon={faBars} style={{ color: "#ffffff"}}/>
+          <FontAwesomeIcon icon={faBars} style={{ color: "#ffffff" }} />
         </button>
 
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item active">
               <a className="nav-link" href="#">
-                Home 
-                {/* <span className="sr-only">(current)</span> */}
+                Home                
               </a>
             </li>
             <li className="nav-item">
